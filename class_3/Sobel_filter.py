25253da2076fc0a1b4220d1bc13df5d61d0c69f5
intelligentracinginc <allen@intelligentracing.com>
2020-05-16 18:59:15 -0700

    Update Sobel_filter.py

--- class_3/Sobel_filter.py
+++ class_3/Sobel_filter.py
@@ -21,21 +21,21 @@ image = image.astype(np.float32)
 
 # Apply Sobel filter\
 f_x = np.array([[1, 0, -1],[2,0,-2], [1, 0, -1]])
-GxImage = np.abs(cv2.filter2D(image, -1, f_x))
-outImage = cv2.normalize(GxImage, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+Ix = np.abs(cv2.filter2D(image, -1, f_x))
+outImage = cv2.normalize(Ix, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
 cv2.imshow('Gx', outImage)
 
 f_y = np.array([[1, 2, 1],[0,0,0], [-1, -2, -1]])
-GyImage = np.abs(cv2.filter2D(image, -1, f_y))
-outImage = cv2.normalize(GyImage, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+Iy = np.abs(cv2.filter2D(image, -1, f_y))
+outImage = cv2.normalize(Iy, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
 cv2.imshow('Gy', outImage)
 
 # Apply omnidirecitional Sobel filter
 smoothFilter = np.ones((7,7))/49.0
 smoothImage = cv2.filter2D(image, -1, smoothFilter)
-GxImage = cv2.filter2D(smoothImage, -1, f_x)
-GyImage = cv2.filter2D(smoothImage, -1, f_y)
-GImage = np.sqrt(GxImage**2 + GyImage**2)
+Ix = cv2.filter2D(smoothImage, -1, f_x)
+Iy = cv2.filter2D(smoothImage, -1, f_y)
+GImage = np.sqrt(Ix**2 + Iy**2)
 GImage = GImage.astype(np.float32)
 outImage = cv2.normalize(GImage, None, 0, 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8UC1)
 cv2.imshow('G', outImage)
