e43e32ce4a32692adc1a247871daa40cb346d29d
intelligentracinginc <allen@intelligentracing.com>
2020-05-25 23:19:21 -0700

    Update floodfill_segmentation.py

--- class_5/floodfill_segmentation.py
+++ class_5/floodfill_segmentation.py
@@ -12,21 +12,32 @@ import os
 # load file
 path = os.path.dirname(os.path.abspath(__file__))
 filename = path + '/highway_video.mp4'
+outputFilename = path + '/rgb_floodfill.avi'
 
 grabber = cv2.VideoCapture(filename)
 fps = int(grabber.get(cv2.CAP_PROP_FPS))
 width = int(grabber.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(grabber.get(cv2.CAP_PROP_FRAME_HEIGHT))
+output = cv2.VideoWriter(outputFilename, cv2.VideoWriter_fourcc('M', 'J', 'P','G'),\
+     fps, (width, height), True)
 
 while(grabber.isOpened()):
     ret, frame = grabber.read()
 
-    seed = (width//2, height-200)
-    # Create gray image and denoise
+    seed = (width//2, height-100)
+    # Flood the frame to segment only the road
     cv2.floodFill(frame, None, seedPoint = seed, newVal = (255, 0, 0), loDiff=(5,5,5), upDiff = (5,5,5))
+    
+    # Augment the illustration of the seed point
+    cv2.circle(frame, seed, 2, (0, 0, 255), cv2.FILLED, cv2.LINE_AA)
 
-    cv2.imshow('Lines', frame)
-    cv2.waitKey(0)
+    output.write(frame)
+
+    cv2.imshow('Road', frame)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
 
 grabber.release()
+output.release()
 cv2.destroyAllWindows()
