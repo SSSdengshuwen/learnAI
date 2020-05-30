2ebf9d1f9e5d6cbd05be0ae91d51aba6dab41707
intelligentracinginc <allen@intelligentracing.com>
2020-05-30 01:07:05 -0700

    Update color_tracking.py

--- class_6/color_tracking.py
+++ class_6/color_tracking.py
@@ -17,6 +17,8 @@ filename = path + '/jump_rope.mp4'
 XMLAddress = path+'/haarcascade_frontalface_alt.xml'
 face_cascade = cv2.CascadeClassifier(XMLAddress) 
 grabber = cv2.VideoCapture(filename) # Take-Home: Change to zero to try for yourself!
+width = int(grabber.get(cv2.CAP_PROP_FRAME_WIDTH))//2
+height = int(grabber.get(cv2.CAP_PROP_FRAME_HEIGHT))//2
 
 trackingStatus = LOST_TRACK
 while (grabber.isOpened()): 
@@ -28,10 +30,7 @@ while (grabber.isOpened()):
         break
 
      # resize the image to 640
-    height, width, channels = frame.shape
-    height = height // 2
-    width = width // 2
-    frame = cv2.resize(frame,(width, height), interpolation = cv2.INTER_CUBIC)
+    frame = cv2.resize(frame, (width, height), interpolation = cv2.INTER_CUBIC)
 
     if trackingStatus == LOST_TRACK:
         # The face is not being tracked, detect
@@ -50,13 +49,33 @@ while (grabber.isOpened()):
                     largestFace = faces[i]
 
             x,y,w,h = largestFace 
-            # To draw a rectangle in a face  
+            faceSizeThreshold = w * h
+
+    if trackingStatus == INIT_TRACK or trackingStatus == UPDATE_TRACK:
+        # Initialize the tracker
+        seed = (x + w//2, y + h//2)
+        mask = np.zeros((height+2, width+2), dtype = np.uint8)
+        fillValue = 255
+        cv2.floodFill(frame, mask, seedPoint = seed, newVal = (255, 0, 0), loDiff=(100,10,5), upDiff = (100,10,5), flags = cv2.FLOODFILL_MASK_ONLY| (fillValue << 8))
+        _, mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
+        cv2.imshow('Mask', mask)
+        #_, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        x, y, w, h = cv2.boundingRect(mask)
+        x = x - 1
+        y = y - 1
+        if w*h<faceSizeThreshold/2 or w*h>faceSizeThreshold*2:
+            # Lost Track
+            trackingStatus = LOST_TRACK
+            # continue
+        else:
+            trackingStatus = UPDATE_TRACK
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
 
-    if trackingStatus == INIT_TRACK:
-        # Initialize the tracker
-        seedPoint = (x + w//2, y + h//2)
-        
+    cv2.imshow('Video',frame)
 
-    if trackingStatus == UPDATE_TRACK:
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
+grabber.release()
+cv2.destroyAllWindows()
